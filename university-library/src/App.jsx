04bc82490ee7dc5bc68d4b270490a741a19e8143
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-02-17 22:45:43 +0530

    first commit

---
+++ university-library/src/App.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+
+function App() {
+
+  return (
+    <>
+   <div>
+         <h6 className="font-extrabold text-5xl">asas</h6>
+   </div>
+    </>
+  )
+}
+
+export default App
